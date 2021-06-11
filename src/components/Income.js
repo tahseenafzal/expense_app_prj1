1ee0db3d985b5365e348a63334d3881976216f61
Tehseen Afzal <abuashja@gmail.com>
2021-06-12 00:51:33 +0500

    Update Income.js

--- src/components/Income.js
+++ src/components/Income.js
@@ -3,9 +3,9 @@ import React from "react";
 const Income = () => {
   return (
     <div>
-      <h1>
-        Income: <br /> $300
-      </h1>
+      <h3 className="income">
+        <span>Income:</span> <span>$600</span>
+      </h3>
     </div>
   );
 };
