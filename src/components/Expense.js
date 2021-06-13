6e625cac96f1ae317934f7cfacd2d0eea43d48a1
Tehseen Afzal <abuashja@gmail.com>
2021-06-14 00:30:56 +0500

    Update Expense.js
    Add expense calculation

--- src/components/Expense.js
+++ src/components/Expense.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
+import { TransContext } from "../TransContext";
 
 const Expense = () => {
+  const { getExpense } = useContext(TransContext);
+
   return (
     <div>
       <h3 className="expense">
-        <span>Expense:</span> <span>$300</span>
+        <span>Expense:</span> <span>${getExpense()}</span>
       </h3>
     </div>
   );
