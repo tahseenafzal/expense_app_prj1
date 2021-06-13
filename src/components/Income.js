3dcf8ae35a5e27a2a621bbad99abc4bffad35119
Tehseen Afzal <abuashja@gmail.com>
2021-06-14 00:31:45 +0500

    Update Income.js
    Add income calculation

--- src/components/Income.js
+++ src/components/Income.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
+import { TransContext } from "../TransContext";
 
 const Income = () => {
+  const { getIncome } = useContext(TransContext);
+
+  // const getIncome = () => {
+  //   let income = 0;
+  //   for (var i = 0; i < trans.length; i++) {
+  //     if (trans[i].amount > 0) {
+  //       income = income + trans[i].amount;
+  //     }
+  //   }
+  //   return income;
+  // };
+
   return (
     <div>
       <h3 className="income">
-        <span>Income:</span> <span>$600</span>
+        <span>Income:</span> <span>${getIncome()}</span>
       </h3>
     </div>
   );
